87f792900d61dd7f89f43a91ec0f8ad926682479
YeakubSadli <yakubsadlil@gmail.com>
2024-11-18 23:34:13 +0600

    add submitanswer endpoint

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegistrationView,LoginView,QuestionListView
+from .views import RegistrationView,LoginView,QuestionListView,QuestionDetailView,SubmitAnswerView
 
 urlpatterns=[
     path("register/", RegistrationView.as_view(),name='register'),
     path("login/", LoginView.as_view(),name='login'),
-    path("questionlist/", QuestionListView.as_view(),name='QuestionList')
+    path("questionlist/", QuestionListView.as_view(),name='QuestionList'),
+    path("question-detail/<int:pk>",QuestionDetailView.as_view(),name="QuestionDetail"),
+    path("submit-answer/",SubmitAnswerView.as_view(),name="SubmitAnswer")
 ]
