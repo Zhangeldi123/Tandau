1e8527ef01fecac5044cdfc97f1a893670d48d6f
YeakubSadli <yakubsadlil@gmail.com>
2024-11-20 18:55:21 +0600

    add homeview for root url

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistrationView,LoginView,QuestionListView,QuestionDetailView,SubmitAnswerView,UserPracticeHistoryView
+from .views import *
 
 urlpatterns=[
     path("register/", RegistrationView.as_view(),name='register'),
